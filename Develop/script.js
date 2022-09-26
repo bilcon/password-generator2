5f0b5ea3b48a1f718cb277cb3ac898fe79904785
Bill Morris <bilcon@swbell.net>
2022-09-26 11:39:55 -0500

    added else if statements for characters used

--- Develop/script.js
+++ Develop/script.js
@@ -108,22 +108,66 @@ get.addEventListener("click", function () {
 // Starts generating password function
 function generatePassword() {
   // Asks for input how many characters
-  enter = parseInt(prompt("How many characters do you want in your password?\n\nEnter a number between 8 and 128\n"));
-  // First if statement for user validation 
+  enter = parseInt(
+    prompt(
+      "How many characters do you want in your password?\n\nEnter a number between 8 and 128\n"
+    )
+  );
+  // First if statement for user validation
   if (!enter || enter < 8 || enter > 128) {
-      alert("You must enter an actual number between 8 and 128");
-      return generatePassword();
+    alert("You must enter an actual number between 8 and 128");
+    return generatePassword();
   } else {
-  // Continues once user input is validated
-  // Start user input prompts
+    // Continues once user input is validated
+    // Start user input prompts
+    confirmLowercase = confirm(
+      "Do you want to use Lowercase letters?\n\nSelect OK for YES or Cancel for NO"
+    );
+    confirmUppercase = confirm(
+      "Do you want to use Uppercase letters?\n\nSelect OK for YES or Cancel for NO"
+    );
+    confirmNumber = confirm(
+      "Do you want to use a number?\n\nSelect OK for YES or Cancel for NO"
+    );
+    confirmCharacter = confirm(
+      "Do you want to use special characters?\n\nSelect OK for YES or Cancel for NO"
+    );
+  }
 
-      confirmLowercase = confirm("Do you want to use Lowercase letters?\n\nSelect OK for YES or Cancel for NO");
-      confirmUppercase = confirm("Do you want to use Uppercase letters?\n\nSelect OK for YES or Cancel for NO");
-      confirmNumber = confirm("Do you want to use a number?\n\nSelect OK for YES or Cancel for NO");
-      confirmCharacter = confirm("Do you want to use special characters?\n\nSelect OK for YES or Cancel for NO");
-  };
+  // First if statement that uses user input prompts to determine choices
+  // Else if for 4 negative options
+  if (
+    !confirmLowercase &&
+    !confirmUppercase &&
+    !confirmNumber &&
+    !confirmCharacter
+  ) {
+    choices = alert("You must select OK for at least one of the choices!");
+    return generatePassword();
+  }
 
-  
+  // Else if for 4 positive options
+  else if (
+    confirmLowercase &&
+    confirmUppercase &&
+    confirmNumber &&
+    confirmCharacter
+  ) {
+    choices = character.concat(alpha, alphaUpper, number, confirmCharacter);
+  }
+
+  // Else if for 3 positive options
+  else if (confirmLowercase && confirmUppercase && confirmNumber) {
+    choices = number.concat(alpha, alphaUpper);
+  } else if (confirmLowercase && confirmUppercase && confirmCharacter) {
+    choices = character.concat(alpha, alphaUpper);
+  } else if (confirmLowercase && confirmNumber && confirmCharacter) {
+    choices = character.concat(alpha, number);
+  } else if (confirmUppercase && confirmNumber && confirmCharacter) {
+    choices = character.concat(alphaUpper, number);
+  }
+}
+
 // Assignment Code
 //var generateBtn = document.querySelector("#generate");
 
